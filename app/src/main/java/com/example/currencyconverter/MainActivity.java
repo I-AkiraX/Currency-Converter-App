95131ec2a3472cde1bc49aa94909370eeccb05d4
I-AkiraX <akira2001official@gmail.com>
2021-07-04 22:12:01 +0530

    Optimised imports in MainActivity.java 04-07-2021 10-11p

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.currencyconverter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.currencyconverter.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+import com.example.currencyconverter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
